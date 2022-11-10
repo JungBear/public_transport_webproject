883b2765b666cd6cc6ffa4379d2e87bf67644eae
JungBear <mod459@naver.com>
2022-11-10 14:12:44 +0900

    게시글 등록 삭제 수정 알람 추가

--- Board/src/main/java/com/study/domain/post/PostController.java
+++ Board/src/main/java/com/study/domain/post/PostController.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.study.common.dto.MessageDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -34,14 +37,16 @@ public class PostController {
         
     // 신규 게시글 생성
     @PostMapping("/post/save.do")
-    public String savePost(final PostRequest params, HttpSession session) {
+    public String savePost(final PostRequest params, HttpSession session, Model model) {
 //    	int writer = (int) session.getAttribute("no");
     	params.setWriterNo(1);
         postService.savePost(params);
         if (params.getType() == 0) {
-        	return "redirect:/post/infolist.do";
+        	 MessageDto message = new MessageDto("게시글 생성이 완료되었습니다.", "/post/infolist.do", RequestMethod.GET, null);
+        	return showMessageAndRedirect(message, model);
         }else {
-        	return "redirect:/post/freelist.do";
+        	 MessageDto message = new MessageDto("게시글 생성이 완료되었습니다.", "/post/freelist.do", RequestMethod.GET, null);
+        	 return showMessageAndRedirect(message, model);
         }
     }
     
@@ -70,24 +75,35 @@ public class PostController {
     
     // 기존 게시글 수정
     @PostMapping("/post/update.do")
-    public String updatePost(final PostRequest params) {
+    public String updatePost(final PostRequest params, Model model) {
     	params.setWriterNo(1);
         postService.updatePost(params);
+        
         if (params.getType() == 0) {
-        	return "redirect:/post/infolist.do";
+        	MessageDto message = new MessageDto("게시글 수정이 완료되었습니다.", "/post/infolist.do", RequestMethod.GET, null);
+        	return showMessageAndRedirect(message, model);
         }else {
-        	return "redirect:/post/freelist.do";
+        	MessageDto message = new MessageDto("게시글 수정이 완료되었습니다.", "/post/freelist.do", RequestMethod.GET, null);
+        	return showMessageAndRedirect(message, model);
         }
     }
     
     // 게시글 삭제
     @PostMapping("/post/delete.do")
-    public String deletePost(@RequestParam final Long id, @RequestParam final int type) {
+    public String deletePost(@RequestParam final Long id, @RequestParam final int type, Model model) {
         postService.deletePost(id);
         if(type == 0) {
-        	return "redirect:/post/infolist.do";
+        	MessageDto message = new MessageDto("게시글 삭제가 완료되었습니다.", "/post/infolist.do", RequestMethod.GET, null);
+        	return showMessageAndRedirect(message, model);
         }else {
-        	return "redirect:/post/freelist.do";
+        	MessageDto message = new MessageDto("게시글 삭제가 완료되었습니다.", "/post/freelist.do", RequestMethod.GET, null);
+        	return showMessageAndRedirect(message, model);
         }
+    }
+    
+    // 사용자에게 메시지를 전달하고, 페이지를 리다이렉트 한다.
+    private String showMessageAndRedirect(final MessageDto params, Model model) {
+        model.addAttribute("params", params);
+        return "common/messageRedirect";
     }
 }
