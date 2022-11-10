2c1dc544d58247dabb013ae1cf7dc114587ce969
lysyeah <dldydtn04@naver.com>
2022-11-10 12:50:48 +0900

    유저컨트롤러 수정

---
+++ Board/src/main/java/com/study/domain/user/UserController.java
@@ -0,0 +1,54 @@
+package com.study.domain.user;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller //현재 클래스를 스프링에서 관리하는 컨트롤러 bean으로 생성
+@RequestMapping("/user/*") //모든 맵핑은 /user/를 상속
+public class UserController {
+    // 로깅을 위한 변수
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+    
+    @Inject
+    UserService memberService;
+    
+    // 01. 로그인 화면 
+    @RequestMapping("login.do")
+    public String login(){
+        return "member/login";    // views/member/login.jsp로 포워드
+    }
+    
+    // 02. 로그인 처리
+    @RequestMapping("loginCheck.do")
+    public ModelAndView loginCheck(@ModelAttribute UserVO vo, HttpSession session){
+        boolean result = memberService.loginCheck(vo, session);
+        ModelAndView mav = new ModelAndView();
+        if (result == true) { // 로그인 성공
+            // main.jsp로 이동
+            mav.setViewName("home");
+            mav.addObject("msg", "success");
+        } else {    // 로그인 실패
+            // login.jsp로 이동
+            mav.setViewName("member/login");
+            mav.addObject("msg", "failure");
+        }
+        return mav;
+    }
+    
+    // 03. 로그아웃 처리
+    @RequestMapping("logout.do")
+    public ModelAndView logout(HttpSession session){
+        memberService.logout(session);
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("member/login");
+        mav.addObject("msg", "logout");
+        return mav;
+    }
+}
