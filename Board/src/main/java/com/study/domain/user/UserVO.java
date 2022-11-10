b3b0769753cd6743c13eb682dda076f28d894ca0
lysyeah <dldydtn04@naver.com>
2022-11-10 10:34:51 +0900

    lys login ver.1

--- Board/src/main/java/com/study/domain/user/UserVO.java
+++ Board/src/main/java/com/study/domain/user/UserVO.java
@@ -1,8 +1,17 @@
 package com.study.domain.user;
 
+import java.sql.Date;
+
 import lombok.Data;
 
 @Data
-public class UserVO {
-	private String name = "test18h57m";
+public class UserVO { //데이터 저장소
+    private String userId;
+    private String userPw;
+    private String userName; 
+    private String userEmail; 
+    private Date userRegdate; // java.sql.Date
+    private Date userUpdatedate;
+ 
+    
 }
