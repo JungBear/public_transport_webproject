be91b55e033a69e0d4982aa031868374f69d0f42
lysyeah <dldydtn04@naver.com>
2022-11-18 16:26:15 +0900

    user response에도 변수 추가

--- Board/src/main/java/com/study/UserRequest.java
+++ Board/src/main/java/com/study/UserRequest.java
@@ -6,13 +6,13 @@ import lombok.Data;
 public class UserRequest {
 	//board 
 	private Long id;          //PK  
-    private String name;
-    private String nickname;
-    private String email;
-    private String pwd;
-    private String pwd_hint;
-    private String pwd_hint_ans;
-    private int gender;
-    private int age;
+    private String name;	//이름
+    private String nickname;	//닉네임
+    private String email;	//이메일
+    private String pwd;	//비밀번호
+    private String pwd_hint;	//비밀번호 힌트 질문
+    private String pwd_hint_ans;	//비밀번호 힌트에 대한 답
+    private int gender;	//성별
+    private int age;	//나이
         
 }
--- Board/src/main/java/com/study/UserResponse.java
+++ Board/src/main/java/com/study/UserResponse.java
@@ -1,11 +1,32 @@
 package com.study;
 
-import java.net.PasswordAuthentication;
+import java.time.LocalDateTime;
 
 import lombok.Data;
 
 @Data
 public class UserResponse {
-	private Long id;            
-    private PasswordAuthentication pwd;   
+	//user 
+	private Long id;//PK  
+    private String name;//이름
+    private String nickname;//닉네임
+    private String email;//이메일
+    private String pwd;	//비밀번호
+    private String pwd_hint;//비밀번호 힌트 질문
+    private String pwd_hint_ans;//비밀번호 힌트에 대한 대답
+   
+    private int gender;//성별
+    private int age;//나이
+    private int exp;//경험치
+    private int del_yn;//삭제여부
+    private int Administrator;//관리자 여부
+    private int attendance_date;//출석일
+    
+    private LocalDateTime signin_date;//로그인 날짜
+    private LocalDateTime now_login_time;//현재 로그인 시간
+    private LocalDateTime recent_login_time;//직전 로그인 시간
+    private LocalDateTime recent_logout_time;//직전 로그아웃 시간
+    private LocalDateTime modified_date;//수정 날짜
+    private LocalDateTime del_date;//삭제한 날짜
+    
 }
