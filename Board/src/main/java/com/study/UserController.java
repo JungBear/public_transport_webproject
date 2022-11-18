d8e6102c9977ee16902b76f93ed17dd0a4c05d46
lysyeah <dldydtn04@naver.com>
2022-11-18 09:46:56 +0900

    로그인과 회원가입 완료/alert꾸미고싶다

--- Board/src/main/java/com/study/UserController.java
+++ Board/src/main/java/com/study/UserController.java
@@ -6,10 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import lombok.RequiredArgsConstructor;
 
 @Controller
+//위에 뜻 = 컨트롤러일 때만.
 @RequiredArgsConstructor
 public class UserController {
-	    
-
 	
 	//로그인 페이지 및 회원가입 페이지
 	@GetMapping("/user/signinandsignup.do")
