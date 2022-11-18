8d3e2fa0c6e75cfec625f0312c75882e39cb6cbe
lysyeah <dldydtn04@naver.com>
2022-11-18 16:54:25 +0900

    주석 달았음.질문사항:DB에는 nickname컬럼이 없다

--- Board/src/main/java/com/study/domain/post/PostResponse.java
+++ Board/src/main/java/com/study/domain/post/PostResponse.java
@@ -12,7 +12,7 @@ public class PostResponse {
     private String title;                  // 제목
     private String content;                // 내용
     private int writerNo;                 // 작성자
-    private String nickname;
+    private String nickname;		// 닉네임
     private int notice;              // 공지글 여부
     private int delYn;              // 삭제 여부
     private LocalDateTime createdDate;     // 생성일시 
