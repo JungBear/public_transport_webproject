ff7589a089c4718efdb1cb8f8058e5ebadfc22d7
JungBear <mod459@naver.com>
2022-11-18 15:54:43 +0900

    컨트롤러의 getcongestionRate함수 파라미터 고치기전 백업

--- Board/src/main/java/com/study/domain/main/MainController.java
+++ Board/src/main/java/com/study/domain/main/MainController.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,7 +19,7 @@ public class MainController {
 	private final MainService MainService;
 	
 
-	@RequestMapping(value = "/")
+	@GetMapping(value = "/")
 	public String main() {
 		return "home";
 	}
