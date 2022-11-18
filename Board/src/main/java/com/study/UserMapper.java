d4bcb2ca4aa1c7ad581741960ff2a8b2bbd9e61d
lysyeah <dldydtn04@naver.com>
2022-11-18 16:37:34 +0900

    mappers 패키지에 usermapper.xml 추가함.

--- Board/src/main/java/com/study/UserMapper.java
+++ Board/src/main/java/com/study/UserMapper.java
@@ -1,8 +1,47 @@
 package com.study;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface UserMapper {
+public interface UserMapper { //mapper.xml을 불러오는 기능
+	
+	/**
+     * 게시글 저장
+     * @param params - 게시글 정보
+     */
+    void id(UserRequest params);
 
+    /**
+     * 게시글 상세정보 조회
+     * @param id - PK
+     * @return 게시글 상세정보
+     */
+    UserResponse findById(Long id);
+    
+    /**
+     * 게시글 수정
+     * @param params - 게시글 정보
+     */
+    void update(UserRequest params);
+
+    /**
+     * 게시글 삭제
+     * @param id - PK
+     */
+    void deleteById(Long id);
+
+    /**
+     * 게시글 리스트 조회
+     * @return 게시글 리스트
+     */
+    List<UserResponse> findAll();
+
+    /**
+     * 게시글 수 카운팅
+     * @return 게시글 수
+     */
+    int count();
+	
 }
